cd361c60ec2eec665cc46fb30b31cc6b63b805cc
Ilya <inshildr@gmail.com>
2026-07-21 11:24:59 +0300

    Мой город

--- 14 - async/14.3 Мой город/scripts.js
+++ 14 - async/14.3 Мой город/scripts.js
@@ -1,23 +1,38 @@
-function getUserLocation(){
+function getMyCoordinates() {
   return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        resolve({
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        });
+      },
+      (error) => {
+        reject(error);
+      }
+    );
   });
 }
 
-const button = document.getElementById("btn");
-const result = document.getElementById("result");
+async function getMyCity() {
+  try {
+    const { latitude, longitude } = await getMyCordinates();
+    
+    const response = await fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
+    );
+    
+    if (!responce.ok) {
+      throw new Error(`Ошибка запроса: ${response.status}`);
+    }
 
-button.addEventListener("click", () => {
-  result.textContent = "Получение координат...";
+    const data = await responce.json();
 
-  getUserLocation()
-    .then((position) => {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
+    console.log(data);
+    console.log(`Город: ${data.city || data.locality}`);
+  } catch(error) {
+    console.error(error);
+  }
+}
 
-      result.textContent = `Широта: ${latitude}, Долгода: ${longitude}`;
-    })
-    .catch(() => {
-      result.textContent = "Не удалось получить координаты";
-    });
-});
+getMyCity()
